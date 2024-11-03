4c47ca53da80acfb7796fb1ec090548f0b25e9ff
Ian July <ianjuly4@gmail.com>
2024-11-03 11:33:43 -0700

    scraper code along

--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -3,19 +3,17 @@ from Course import Course
 class Test_Course:
     '''Course in Course.py'''
 
-
-
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
-        assert(course.title == "Programming Robots for Outer Space")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        assert(course.title == "Programming Robots for Outer Space", course.schedule == "Full-Time", course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
-        assert(course.schedule == "Full-Time")
+        course = Course(schedule="Full-Time",title="Programming Robots for Outer Space",description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        assert(course.schedule == "Full-Time", course.title == "Programming Robots for Outer Space", course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
-        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover", title="Programming Robots for Outer Space", schedule="Full-Time")
+        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover", course.title == "Programming Robots for Outer Space", course.schedule == "Full-Time")
